b4c846f61d7e19fa618964becb90ec4d616ade3b
Claudio Figueroa <claudio.figueroa@medeiconsultoria.com.br>
2021-10-04 13:27:11 -0300

    second layer of security (encryption)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const encrypt = require("mongoose-encryption");
 const _ = require('lodash');
 
 
@@ -12,7 +13,22 @@ app.set('view engine', 'ejs');
 
 mongoose.connect("mongodb://localhost:27017/authDB");
 
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
 
+}); 
+
+const secret = "ThisisourLittleSecret.";
+userSchema.plugin(encrypt, {secret:secret , encryptedFields: ["password"]});
+
+const User = new mongoose.model('User', userSchema);
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -23,12 +39,39 @@ app.route("/login")
         res.render("login");
     })
 
-    .post();
+    .post(function(req, res){
+        User.findOne({email:req.body.username}, function(err, user){
+            if(user){
+                if(user.password === req.body.password)
+                res.render("Secrets");
+            }
+            else{
+                res.send(err.message);
+            }
+
+        })
+    });
 
 
 app.route("/register")
     .get(function(req, res) {
         res.render("register")
+    })
+
+    .post(function(req,res){
+        const newUser = new User({
+           email: req.body.username,
+           password: req.body.password
+        });
+
+        newUser.save(function(err){
+            if(err){
+                res.send(err.message);
+            }
+            else{
+                res.render("secrets");
+            }
+        })
     })
 
 
